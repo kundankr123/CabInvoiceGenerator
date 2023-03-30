de684b84785dc257ac90df4fae6c016b8dd122ae
kundan <goswamikundan123@gmail.com>
2023-03-30 15:18:32 +0530

    Adding file which Calcuates fare and display it.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to master branch of Cab Invoice Generator.");
+            //Console.WriteLine("Welcome to master branch of Cab Invoice Generator.");
+
+            double distance, time, fare;
+
+            Console.WriteLine("Enter the distance travelled in km: ");
+            distance = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the time taken in minutes: ");
+            time = Convert.ToDouble(Console.ReadLine());
+
+            fare = (distance * 10) + (time * 1);
+
+            if (fare < 5)
+            {
+                fare = 5;
+            }
+
+            Console.WriteLine("Total fare: Rs. " + fare);
         }
     }
 }
