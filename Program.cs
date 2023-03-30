bf1e6c18c16fd9947ccb79afa5625d831e40a540
kundan <goswamikundan123@gmail.com>
2023-03-30 14:57:13 +0530

    Adding file which displays Greeting message on Master Branch.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to master branch of Cab Invoice Generator.");
+        }
+    }
+}
